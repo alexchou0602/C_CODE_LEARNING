c0f03b8e1937c44923597bac57928ea2232adf4e
alexchou0602 <alexchou_ruc@hotmail.com>
2023-12-22 22:53:34 +0800

    if语句尝试

--- test_20231222/test_20231222/test_1.c
+++ test_20231222/test_20231222/test_1.c
@@ -5,30 +5,85 @@
 //    return 0;
 //}
 
+//#include<stdio.h>
+//int main()
+//{
+//	const int MINOR = 35;
+//	int age = 0;
+//	printf("输入你的年龄：");
+//	scanf("%d", &age);
+//	printf("你的年龄是%d岁。\n", age);
+//
+//	if (age < MINOR)
+//	{
+//		if (age <= 0)
+//		{
+//			printf("你挺有病的。\n");
+//		}
+//		else
+//		{
+//			printf("你还挺年轻的。\n");
+//		}
+//	}
+//	else
+//		{
+//			printf("你挺成熟的。\n");
+//		}
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int price = 0;
+//	int bill = 0;
+//	printf("请输入货款金额：");
+//	scanf("%d", &price);
+//	printf("请输入票面金额：");
+//	scanf("%d", &bill);
+//	if(bill>price)
+//	{
+//		printf("应该找零：%d\n", bill - price);
+//	}
+//	else
+//	{
+//		printf("您的钱不够。\n");
+//	}
+//
+//
+//	return 0;
+//}
+
 #include<stdio.h>
 int main()
 {
-	const int MINOR = 35;
-	int age = 0;
-	printf("输入你的年龄：");
-	scanf("%d", &age);
-	printf("你的年龄是%d岁。\n", age);
-
-	if (age < MINOR)
+	int a = 0;
+	int b = 0;
+	int max = 0;
+	printf("输入两个整数：");
+	scanf("%d %d", &a, &b);
+	if (a > b)
 	{
-		if (age <= 0)
+		max = a;
+		printf("大值为%d\n", max);
+
+	}
+	else
+	{
+		if (a == b)
 		{
-			printf("你的年龄输入有误。\n");
+			printf("两值相同\n");
+
 		}
 		else
 		{
-			printf("你还挺年轻的。\n");
+			max = b;
+			printf("大值为%d\n", max);
 		}
 	}
-	else
-		{
-			printf("你挺成熟的。\n");
-		}
+
+
 
 	return 0;
 }
