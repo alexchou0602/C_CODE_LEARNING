b6c46379556fec8dd6a0e408f840d884f1e9adc5
alexchou0602 <alexchou_ruc@hotmail.com>
2023-12-22 22:05:55 +0800

    if语句尝试

--- test_20231222/test_20231222/test_1.c
+++ test_20231222/test_20231222/test_1.c
@@ -1,6 +1,34 @@
+//#include<stdio.h>
+//int main ()
+//{
+//	printf("hello world!\n");
+//    return 0;
+//}
+
 #include<stdio.h>
-int main ()
+int main()
 {
-	printf("hello world!\n");
-		return 0;
+	const int MINOR = 35;
+	int age = 0;
+	printf("输入你的年龄：");
+	scanf("%d", &age);
+	printf("你的年龄是%d岁。\n", age);
+
+	if (age < MINOR)
+	{
+		if (age <= 0)
+		{
+			printf("你的年龄输入有误。\n");
+		}
+		else
+		{
+			printf("你还挺年轻的。\n");
+		}
+	}
+	else
+		{
+			printf("你挺成熟的。\n");
+		}
+
+	return 0;
 }
