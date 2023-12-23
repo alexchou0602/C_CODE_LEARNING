f24aed8cf3cc31cc2024a1978cd12916852dfcda
alexchou0602 <alexchou_ruc@hotmail.com>
2023-12-23 09:43:12 +0800

    while循环

--- test_20231222/test_20231222/test_1.c
+++ test_20231222/test_20231222/test_1.c
@@ -1,11 +1,13 @@
-//#include<stdio.h>
+#include<stdio.h>
+//hello world
 //int main ()
 //{
 //	printf("hello world!\n");
 //    return 0;
 //}
 
-//#include<stdio.h>
+
+//输入不同年龄了获得不同答案
 //int main()
 //{
 //	const int MINOR = 35;
@@ -33,7 +35,7 @@
 //	return 0;
 //}
 
-//#include <stdio.h>
+//输入货款和票面计算找零
 //int main()
 //{
 //	int price = 0;
@@ -50,39 +52,93 @@
 //	{
 //		printf("您的钱不够。\n");
 //	}
+//	return 0;
+//}
+
+//两个整数比大小
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int max = 0;
+//	printf("输入两个整数：");
+//	scanf("%d %d", &a, &b);
+//	if (a > b)
+//	{
+//		max = a;
+//		printf("大值为%d\n", max);
+//	}
+//	else
+//	{
+//		if (a == b)
+//		{
+//			printf("两值相同\n");
+//		}
+//		else
+//		{
+//			max = b;
+//			printf("大值为%d\n", max);
+//		}
+//	}
+//	return 0;
+//}
+
+//三个整数比大小
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	int max = 0;
+//
+//	printf("输入三个整数：");
+//	scanf("%d %d %d", &a, &b, &c);
+//	if (a > b)
+//	{
+//		if (a > c)
+//		{
+//			max = a;
+//		}
+//		else
+//		{
+//			max = c;
+//		}
+//	}
+//	else
+//	{
+//		if (b > c)
+//		{
+//			max = b;
+//		}
+//		else
+//		{
+//			max = c;
+//		}
+//	}
+//	printf("最大值是%d。", max);
 //
 //
 //	return 0;
 //}
 
-#include<stdio.h>
+//输入一个整数计算是几位数(理解复合赋值）
 int main()
 {
 	int a = 0;
 	int b = 0;
-	int max = 0;
-	printf("输入两个整数：");
-	scanf("%d %d", &a, &b);
-	if (a > b)
+	int n = 0;
+	printf("请输入一个整数：");
+	scanf("%d", &a);
+	printf("您输入的整数是%d。\n", a);
+
+	n++;
+	b=a / 10;
+	while (b>0)
 	{
-		max = a;
-		printf("大值为%d\n", max);
-
+		n++;
+		b /= 10;
 	}
-	else
-	{
-		if (a == b)
-		{
-			printf("两值相同\n");
-
-		}
-		else
-		{
-			max = b;
-			printf("大值为%d\n", max);
-		}
-	}
-
+	printf("这个整数是%d位数。\n", n);
 
 
 	return 0;
