613de347e2e7074a18da0c6ad01a78869548e7d8
alexchou0602 <alexchou_ruc@hotmail.com>
2023-12-29 23:36:11 +0800

    函数的使用，传值与返回值

--- test_20231222/test_20231222/test_1.c
+++ test_20231222/test_20231222/test_1.c
@@ -530,26 +530,3 @@ int main()
 		mask /= 10;
 	} while (mask > 0);
 	
-=======
-
-//函数的使用
-void sum(int begin, int end)
-{
-	int i;
-	int sum = 0;
-		for (i = begin; i <= end; i++ )
-		{
-			sum += i;
-		}
-	printf("%d到%d的和是%d\n", begin, end, sum);
-}
-
-int main()
-{
-	sum(1, 10);
-	sum(20, 30);
-	sum(35, 45);
->>>>>>> Stashed changes
-
-	return 0;
-}
