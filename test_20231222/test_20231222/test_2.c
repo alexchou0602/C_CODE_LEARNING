36f02cc92a28b8c164bb5aa00d243f317f70eff9
alexchou0602 <alexchou_ruc@hotmail.com>
2024-01-07 22:26:50 +0800

    指针的运算

--- test_20231222/test_20231222/test_2.c
+++ test_20231222/test_20231222/test_2.c
@@ -25,33 +25,53 @@
 //找出一个数组中的最大值
 //指针作用为从函数中返回多个值
 
-void minmax(int a[], int len, int* min, int* max);
+//void minmax(int a[], int len, int* min, int* max);
+//
+//int main(void)
+//{
+//	int a[] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,45,65 };
+//	int min;
+//	int max;
+//	minmax(a, sizeof(a) / sizeof(a[0]), &min, &max);
+//	printf("min=%d,max=%d\n", min, max);
+//
+//
+//	return 0;
+//}
+//
+//void minmax(int a[], int len, int* min, int* max)
+//{
+//	int i;
+//	*min = *max = a[0];
+//	for (i = 1; i < len; i++)
+//	{
+//		if (a[i] < *min)
+//		{
+//			*min = a[i];
+//		}
+//		if (a[i > *max])
+//		{
+//			*max = a[i];
+//		}\
 
+//
+
+
+//指针加1，指的是加上一个该指针所指变量类型的大小。
 int main(void)
 {
-	int a[] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,45,65 };
-	int min;
-	int max;
-	minmax(a, sizeof(a) / sizeof(a[0]), &min, &max);
-	printf("min=%d,max=%d\n", min, max);
-
-
+	char ac[] = { 0,1,2,3,4,5,6,7,8,9 };
+	char* p = &ac[0];
+	char* p1 = &ac[5];
+	printf("p=%p\n", p);
+	printf("p+1=%p\n", p + 1);
+	printf("p1-p=%d\n", p1 - p);
+	int ai[] = { 0,1,2,3,4,5,6,7,8,9 };
+	int* q = &ai[0];
+	char* q1 = &ai[5];
+	printf("q=%p\n", q);
+	printf("q+1=%p\n", q+1);
+	printf("q1=%p\n", q1);
+	printf("q1-q=%d\n", q1 - q);
 	return 0;
-}
-
-void minmax(int a[], int len, int* min, int* max)
-{
-	int i;
-	*min = *max = a[0];
-	for (i = 1; i < len; i++)
-	{
-		if (a[i] < *min)
-		{
-			*min = a[i];
-		}
-		if (a[i > *max])
-		{
-			*max = a[i];
-		}
-	}
 }
